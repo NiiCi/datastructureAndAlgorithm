543dafcf749ec4a97cb2d093c03cf984a6322f02
niici <1125382627@qq.com>
2021-10-20 17:20:24 +0800

    1. 斐波那契查找；

--- src/main/java/com/niici/study/algorithm/search/FibonacciSearch.java
+++ src/main/java/com/niici/study/algorithm/search/FibonacciSearch.java
@@ -1,5 +1,10 @@
 package com.niici.study.algorithm.search;
 
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 斐波那契(黄金分割点)查找
  *  黄金分割点是指把一条线段分割为两部分，使其中一部分与全长之比等于另一部分与与这部分之比
@@ -15,9 +20,17 @@ package com.niici.study.algorithm.search;
  *    1. 由斐波那契数列F(k) = F(k-1) + F(k-2)，可以得到 F(k)-1 = (F(k-1)-1) + (F(k-2)-1) + 1，
  *    该式说明，只要顺序表的长度为F(k)-1，则可以将该表分为长度为F(k-1)-1 和 F(k-2)-1两段，而中间位置为mid=low+F(k-1)-1
  *    2. 类似的，每一子段也可以用相同的方式分割
- *    3. 但顺序表擦汗高难度n不一定刚好等于F(k)-1，所以需要将原来的顺序表长度增加至F(k)-1，这里的k值只要能使得F(k)-1恰好大于或者等于n即可，
+ *    3. 但顺序表的长度n不一定刚好等于F(k)-1，所以需要将原来的顺序表长度增加至F(k)-1，这里的k值只要能使得F(k)-1恰好大于或者等于n即可，
  *
  * @author niici
  */
 public class FibonacciSearch extends BaseSearch {
+    public static void main(String[] args) {
+        int[] arr = {1, 8, 10, 89, 1000, 1234};
+    }
+
+    public List<Integer> search() {
+        List<Integer> objects = Lists.newArrayList();
+        return objects;
+    }
 }
