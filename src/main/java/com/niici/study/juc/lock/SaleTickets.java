c1d08270406f6d89d3c7c6bb97708c0f681db404
niici <1125382627@qq.com>
2021-10-21 14:13:03 +0800

    1. ReentranLock售票例子；

--- src/main/java/com/niici/study/juc/lock/SaleTickets.java
+++ src/main/java/com/niici/study/juc/lock/SaleTickets.java
@@ -10,6 +10,14 @@ import java.util.concurrent.locks.ReentrantLock;
  * ReentranLock 售票例子
  * 可重入锁：可重复获取锁
  *
+ * 多线程变成步骤：
+ * 1. 创建资源类，在资源类创建属性和操作方法
+ * 2. 在资源类操作方法：
+ *  1) 判断
+ *  2) 处理
+ *  3) 通知
+ * 3. 创建多个现成，调用资源类的操作方法
+ *
  * @author niici
  */
 public class SaleTickets {
